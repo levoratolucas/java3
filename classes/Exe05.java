b1add7e24b15feec880dbd0a7523d7eff413bdec
Lucas Levorato <lucaslevorato@gmail.com>
2024-03-25 20:26:35 -0300

    exe05

--- classes/Exe05.java
+++ classes/Exe05.java
@@ -1,7 +1,6 @@
 // Exe05.java 
 // 5.  A  Loja  Mamão  com  Açúcar  está  vendendo  seus  produtos  em  5  (cinco)  prestações  sem  juros. 
 // Faça um programa que receba um valor de uma compra e mostre o valor das prestações.
-
 package classes;
 
 public class Exe05 {
@@ -10,6 +9,5 @@ public class Exe05 {
         float valorCompra=Tools.scanffloat();
         Loja loja = new Loja(valorCompra);
         Tools.println(loja.calcular());
-    }
-    
+    }    
 }
